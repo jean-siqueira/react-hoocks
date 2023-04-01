766858177cf3eff940e96ac89ecee9e880d6c789
jean-siqueira <jean.siqueira@outlook.com.br>
2023-04-01 09:50:18 -0300

    UseState

---
+++ .history/src/views/examples/UseState_20230401092748.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+const UseState = (props) => {
+
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className="UseState">
+            <PageTitle
+                title="Hook UseState"
+                subtitle="Estado em componentes funcionais!"
+            />
+            <SectionTitle title="Exercício #01" />
+        </div>
+    )
+}
+
+export default UseState
---
+++ .history/src/views/examples/UseState_20230401092819.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+const UseState = (props) => {
+
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className="UseState">
+            <PageTitle
+                title="Hook UseState"
+                subtitle="Estado em componentes funcionais!"
+            />
+            <SectionTitle title="Exercício #01" />
+
+            <SectionTitle title="Exercício #02" />
+        </div>
+    )
+}
+
+export default UseState
